ee6f6a64696902cea8233aa20a50c69e5f4f7fbb
Anmol <anmolnagvanshi04@gmail.com>
2020-02-29 05:13:26 +0530

    Commit at 05:14AM at SIT

--- donate/urls.py
+++ donate/urls.py
@@ -4,7 +4,8 @@ from . import views
 urlpatterns = [
     path('', views.home, name="user-home"),
     path('register', views.RegisterUser.as_view(), name="user-register"),
-    path('results/', views.FetchResultsCity.as_view(), name="user-results"),
-    path('results/group', views.FetchResultsGroup.as_view(), name="user-results-group"),
+    path('results/', views.FetchResultsCity.as_view(), name="bloodbank-results"),
+    path('results/group', views.FetchResultsGroup.as_view(), name="bloodbank-results-group"),
+    path('results/group/users', views.FetchResultsByUsersGroup.as_view(), name="user-results-group"),
 ]
 
