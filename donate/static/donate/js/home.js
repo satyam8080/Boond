5f70a5b62ddc6010d2adcb1ec64592c0daebab08
Raj Vashishtha <vashiraj2000@gmail.com>
2020-02-29 01:48:43 +0530

    Ready

--- donate/static/donate/js/home.js
+++ donate/static/donate/js/home.js
@@ -1,3 +1,5 @@
+
+
 $(document).ready(function(){
     var groups = ["A-","A+","B-","B+","AB-","AB+","O-","O+"];
     
@@ -132,5 +134,85 @@ $(document).ready(function(){
 
         }
     });//end click function
+
+$("#register_btn").click(function(){
+  $("#register_form").css("display","block")
+  document.getElementById("register_form").scrollIntoView(true);
+})
+
+
+
+var arr = ['Andhra Pradesh',
+'Arunachal Pradesh',
+'Assam',
+'Bihar',
+'Chhattisgarh',
+'Goa',
+'Gujarat',
+'Haryana',
+'Himachal Pradesh',
+'Jammu and Kashmir',
+'Jharkhand',
+'Karnataka',
+'Kerala',
+'Madhya Pradesh',
+'Maharashtra',
+'Manipur',
+'Meghalaya',
+'Mizoram',
+'Nagaland',
+'Odisha',
+'Punjab',
+'Rajasthan',
+'Sikkim',
+'Tamil Nadu',
+'Telangana',
+'Tripura',
+'Uttarakhand',
+'Uttar Pradesh',
+'West Bengal',
+'Andaman and Nicobar Islands',
+'Chandigarh',
+'Dadra and Nagar Haveli',
+'Daman and Diu',
+'Delhi',
+'Lakshadweep',
+'Puducherry'];
+
+for(var i=0 ;i<arr.length;i++){
+    var str = "<option value='"+(i+3)+"' >"+arr[i]+"</option>"
+    $("#selectState").append(str);
+}
+function get_address_coord(location){
+  var state_selected = $("#selectState option:selected").text();
+  var city_select = $("#selectCity option:selected").text();
+  var pin = $("#pincode").val();
+  location = location+","+city_select+","+state_selected+","+pin;
+  axios.get("https://maps.googleapis.com/maps/api/geocode/json",{
+              params:{
+                  address : location,
+                  key : 'AIzaSyBVHC0B-6mFNuMk0lBnOmc-JIJojPxKc2Q'
+              }
+          }).then(function(response){
+              var lati = response.data.results[0].geometry.location.lat;
+              var lngi = response.data.results[0].geometry.location.lng;
+              console.log(lati,lngi)
+              $("#lat").val(lati)
+              $("#lng").val(lngi)
+          }).catch(function(error){
+              alert("Catch")
+              console.log(error+" address not found")
+          })
+}
+$("#pincode").focusout(function(){
+  var location = $("#address").val();
+  console.log("working")
+  get_address_coord(location);
+})
+$("#close").click(function(){
+  $("#register_form").css("display","none");
+})
+
+
 
 });//end document ready
